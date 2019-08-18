795e65451e2a4ffe95c450a79b9411f0c47f2e57
Tyler Williams <tyler@ogdenstudios.xyz>
2019-08-17 21:58:03 -0600

    update navbar styles and home page stuff

---
+++ js/hamburger.js
@@ -0,0 +1,19 @@
+(function () {
+    var hamburger;
+    var navbar;
+    document.addEventListener('DOMContentLoaded', function () {
+        hamburger = document.getElementById('hamburger');
+        navbar = document.getElementById('navbar');
+        hamburger.addEventListener('click', toggle);
+    });
+
+    function toggle() {
+        if (hamburger.classList.contains('is-active')) {
+            hamburger.classList.remove('is-active');
+            navbar.classList.remove('is-active');
+        } else {
+            hamburger.classList.add('is-active');
+            navbar.classList.add('is-active');
+        }
+    }
+})();
