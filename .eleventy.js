a16055d684bf6a98e598a0ade88b557f19d51cde
Tyler Williams <tyler@ogdenstudios.xyz>
2019-12-09 22:06:28 -0700

    start adding gulp and stuff

--- .eleventy.js
+++ .eleventy.js
@@ -3,5 +3,9 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
     eleventyConfig.addLayoutAlias('page', 'layouts/page.html');
     eleventyConfig.addLayoutAlias('post', 'layouts/post.html');
-
+    return {
+      dir: {
+        input: "src"
+      }
+    };
   };
