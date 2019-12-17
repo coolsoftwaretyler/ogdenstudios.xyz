6645cad035775592f15efd12bb8444585bc26496
Tyler Williams <tyler@ogdenstudios.xyz>
2019-12-17 10:24:15 -0700

    move sass into stylesheet

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
-module.exports = function(eleventyConfig) {
+const CleanCSS = require("clean-css");
 
-    eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
-    eleventyConfig.addLayoutAlias('page', 'layouts/page.html');
-    eleventyConfig.addLayoutAlias('post', 'layouts/post.html');
+module.exports = function (eleventyConfig) {
 
-  };
+  eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
+  eleventyConfig.addLayoutAlias('page', 'layouts/page.html');
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.html');
+
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+};
