e799bb88d2a70228edd0473c931a8f4266eab005
Tyler Williams <tyler@ogdenstudios.xyz>
2019-12-17 11:54:12 -0700

    some cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addLayoutAlias('page', 'layouts/page.html');
   eleventyConfig.addLayoutAlias('post', 'layouts/post.html');
 
+  eleventyConfig.addPassthroughCopy("src/fonts");
+  eleventyConfig.addPassthroughCopy("src/img");
+
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
